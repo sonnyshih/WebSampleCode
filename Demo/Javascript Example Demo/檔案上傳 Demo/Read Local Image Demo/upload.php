60d955fa657c47e68a8c1afd47ea41047dbf247b
sonnyshih <sonnyshih@gmail.com>
2017-07-18 16:07:22 +0800

    add the jquery + ajax upload example

---
+++ Demo/Javascript Example Demo/檔案上傳 Demo/Read Local Image Demo/upload.php
@@ -0,0 +1,51 @@
+<?php
+
+//  define('PATH_ROOT', dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
+    define('PATH_ROOT', dirname(__DIR__).DIRECTORY_SEPARATOR);
+    define('PATH_UPLOAD', PATH_ROOT."upload".DIRECTORY_SEPARATOR);
+    
+//     echo PATH_UPLOAD;
+    
+
+    foreach ($_FILES["attachFile"]["error"] as $key => $error) {
+        // UPLOAD_ERR所有說明: http://php.net/manual/en/features.file-upload.errors.php
+        if ($error == UPLOAD_ERR_OK) {
+            
+            
+            $tmp_name = $_FILES["attachFile"]["tmp_name"][$key];
+
+            // basename() may prevent filesystem traversal attacks;
+            // further validation/sanitation of the filename may be appropriate
+            $uploadfile = PATH_UPLOAD . basename($_FILES["attachFile"]["name"][$key]);
+            move_uploaded_file($tmp_name, $uploadfile);
+        }
+    }
+    
+    
+    // 使用form 上傳
+    if (isset($_POST['uploadForm'])) {
+        
+        echo "<pre>_FILES = " . print_r($_FILES['attachFile'], TRUE). "</pre>";
+        
+        
+    } else {
+        
+        $jsonArray = array(
+               'response' => $_POST['hello'],
+            'file number' => sizeof($_FILES['attachFile'])
+        );
+
+        $arttachFile = array();
+        foreach ($_FILES['attachFile']['name'] as $key => $value){
+            $arttachFile[] = $value;
+        } 
+        
+        $jsonArray['attachFile'] = $arttachFile;
+        
+        echo json_encode($jsonArray);
+        
+    }
+
+    
+    
+?>
