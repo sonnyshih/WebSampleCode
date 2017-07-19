f7ffc12c34d837e79dbcc2de4c089936aaa65846
sonnyshih <sonnyshih@gmail.com>
2017-07-19 17:00:52 +0800

    add vzaar demo

---
+++ Demo/Code/PHP/Third Party Lib/vzaar/test.php
@@ -0,0 +1,19 @@
+<?php
+  require_once 'vzaar/Vzaar.php';
+
+    header ('Content-Type: text/html; charset=utf-8');
+    Vzaar::$secret = 'interwood';
+    Vzaar::$token = 'RiSIDEQptsUOo4fnazzquZoVLTvPTjJdeDkhQFzUUdI';
+    $user = Vzaar::whoAmI();
+    echo $user."<br>";
+
+    var_dump(Vzaar::getUserDetails($user));
+
+    echo "<br>";
+    $accountDetails = Vzaar::getAccountDetails(1);
+    var_dump($accountDetails);
+
+    echo "<br>";
+ 
+	print_r(Vzaar::getVideoList($user, true, 10));
+?>
---
+++ Demo/Code/PHP/Third Party Lib/vzaar/vzaar/Constants.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * vzaar constants
+ */
+class Constants
+{
+	const Version  = '1.3.1';
+  const Uploader = 'php-1.3.1';
+}
+?>
