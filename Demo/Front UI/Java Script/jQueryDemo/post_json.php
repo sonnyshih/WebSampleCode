afb048ea88bc1225ef8ae6521f97bbf9254bb51f
sonnyshih <sonnyshih@gmail.com>
2018-04-26 00:04:14 +0800

    1. Modify jQueryPostJSONDemo.html 2. Modify post_json.php

--- Demo/Front UI/Java Script/jQueryDemo/post_json.php
+++ Demo/Front UI/Java Script/jQueryDemo/post_json.php
@@ -29,8 +29,20 @@
         return;
     }
     
+
+    if(isset($_POST['hello'])){
+    	$hello = $_POST['hello'];
+    }
+    if(isset($_GET['hello'])){
+    	$hello = $_GET['hello'];
+    }
+    
+    if (!empty($hello)) {
+    	$array = json_decode($_POST["hello"], true);
+    }
+    
     // 可將獲取的 POST 表單資料，儲存到資料庫（該部分未實作）
     
     // 儲存成功，返回員工姓名
-    echo json_encode(array('name' => $name));
+    echo json_encode(array('name' => $name, 'child' => $array["child"]));
 ?>
